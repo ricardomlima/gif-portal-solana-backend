65bd3979d5e63e0570e8256881c1c48f94e5688e
Ricardo M <ricardolima89@gmail.com>
2022-10-28 13:41:28 -0300

    storing state in users wallet

--- programs/gif-portal-solana-backend/src/lib.rs
+++ programs/gif-portal-solana-backend/src/lib.rs
@@ -5,10 +5,31 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod gif_portal_solana_backend {
     use super::*;
+
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+
+        // Gets the account reference
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Initializes total amount of gifs
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
+// Attaches some variables to the context of StartStuffOff
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// What do we want to store in the account
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
