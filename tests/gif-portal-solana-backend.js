f5680c37468deda6fe54c60af9fafaac6ea1b257
Ricardo M <ricardolima89@gmail.com>
2022-10-28 13:41:45 -0300

    testing state storage in wallet

--- tests/gif-portal-solana-backend.js
+++ tests/gif-portal-solana-backend.js
@@ -1,13 +1,31 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = require('@solana/web3.js');
 
 const main = async () => {
     console.log("🚀 Starting tests...");
 
-    anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
     const program = anchor.workspace.GifPortalSolanaBackend;
-    const tx = await program.rpc.startStuffOff();
+
+    // create a key pair for our test program to use
+    const baseAccount = anchor.web3.Keypair.generate();
+
+
+    const tx = await program.rpc.startStuffOff({
+        accounts: {
+            baseAccount: baseAccount.publicKey,
+            user: provider.wallet.publicKey,
+            systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+    });
 
     console.log("📝 Your transaction signature", tx);
+
+    // get account data
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('GIF count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
